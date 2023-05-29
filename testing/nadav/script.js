31bb0474cf4c911545cd422187a00a0456c8ca91
nadav chen <nadav2282@gmail.com>
2023-05-29 14:01:28 +0300

    added index.html

--- testing/nadav/script.js
+++ testing/nadav/script.js
@@ -1,29 +1,10 @@
-// Sample product data
-const products = [
-  {
-    id: 1,
-    name: 'Product 1',
-    price: 10,
-    image: 'product1.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  {
-    id: 2,
-    name: 'Product 2',
-    price: 20,
-    image: 'product2.jpg',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  // Add more products as needed
-];
-
 // Function to display products
 function displayProducts() {
   const productContainer = document.querySelector('main');
   
   products.forEach((product) => {
     const productElement = document.createElement('div');
-    productElement.classList.add('product');
+    productElement.classList.add('product', 'slide-in');
     
     productElement.innerHTML = `
       <img src="${product.image}" alt="${product.name}">
@@ -33,4 +14,9 @@ function displayProducts() {
       <button onclick="addToCart(${product.id})">Add to Cart</button>
     `;
     
-    productContainer
+    productContainer.appendChild(productElement);
+  });
+
+  const header = document.querySelector('header');
+  header.classList.add('fade-in');
+}
