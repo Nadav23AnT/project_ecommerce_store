4a17c7a625fe4086d60b4bae386c1af86b7c8471
NadavChen <nadav2282@gmail.com>
2023-06-14 15:36:13 +0300

    Added Product Screen

--- client/src/Scripts/app.js
+++ client/src/Scripts/app.js
@@ -1,4 +1,10 @@
 import HomeScreen from './homeScreen.js'
+import ProductScreen from './productScreen.js';
+
+const routes = {
+  "/": HomeScreen,
+  "/product/:id": ProductScreen,
+}
 const router = () => { 
   const main = document.getElementById('main-container');
   main.innerHTML = HomeScreen.router();
