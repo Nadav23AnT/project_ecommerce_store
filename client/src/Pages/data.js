5581eb4f9f94b00a6dca51b5848f0b9ca97b4982
arielaviv <80781502+arielaviv@users.noreply.github.com>
2023-06-14 15:11:23 +0300

    created data.js and app.js

---
+++ client/src/Pages/data.js
@@ -0,0 +1,51 @@
+export default {
+  products: [
+    {
+      id: '1',
+      name: 'XII Series 17.3"',
+      category: 'Laptops',
+      image: '../Assets/Images/Products/p1.png',
+      price: 400,
+      rating: 5,
+      numReviews: 10,
+      countInStock: 6,
+      brand: 'Quantum Tech',
+    },
+
+    {
+      id: '2',
+      name: 'XI Series 15.6"',
+      category: 'Laptops',
+      image: '../Assets/Images/Products/p2.png',
+      price: 350,
+      rating: 4.9,
+      numReviews: 8,
+      countInStock: 6,
+      brand: 'Quantum Tech',
+    },
+
+    {
+      id: '3',
+      name: 'XX Series 14"',
+      category: 'Laptops',
+      image: '../Assets/Images/Products/p3.png',
+      price: 350,
+      rating: 4.8,
+      numReviews: 7,
+      countInStock: 20,
+      brand: 'Quantum Tech',
+    },
+
+    {
+      id: '4',
+      name: 'Logitech G502 Wired',
+      category: 'Mouse',
+      image: '../Assets/Images/Products/p4.png',
+      price: 100,
+      rating: 5,
+      numReviews: 10,
+      countInStock: 6,
+      brand: 'Logitech',
+    },
+  ],
+};
